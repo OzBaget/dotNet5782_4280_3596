68a4ea443bfe5ecff9ffc83f1d32b4ad862ca28e
yoav <yoavsc4@gmail.com>
2021-10-03 21:01:55 +0300

    Initial code+Add Welcome functions

---
+++ Targil0/Program4280.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome4280();
+            Welcome3596();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome3596();
+
+        private static void Welcome4280()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
