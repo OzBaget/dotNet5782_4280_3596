92bf435542f0805b60f8cc83ad0eb797de7952c7
ohad <ohadsc123@gmail.com>
2021-10-14 17:36:38 +0300

    Add Customer struct

---
+++ DAL/IDAL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        public struct Customer
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Phone { get; set; }
+            public double Lng { get; set; }
+            public double Lat { get; set; }
+
+            public override string ToString()
+            {
+                return $"ID: {Id}.\n" +
+                    $"Name: {Name}\n" +
+                    $"Phone Number: {Phone}\n" +
+                    $"Position: {Lng},{Lat}";
+            }
+        }
+    }
+}
