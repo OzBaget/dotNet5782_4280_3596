29b859d298573a14af4053c17811a2c2df471e16
ohad <ohadsc123@gmail.com>
2021-10-18 22:00:19 +0300

    Add constructor

--- DAL/Drone.cs
+++ DAL/Drone.cs
@@ -10,6 +10,16 @@ namespace IDAL
     {
         public struct Drone
         {
+            public Drone(string model, WeightCategories maxWeight)
+            {
+                Random r = new Random();
+                Id = r.Next();
+                Model = model;
+                MaxWeight = maxWeight;
+
+                Status = DroneStatuses.Available;
+                Battery = 100;
+            }
             public int Id { get; set; }
             public string Model { get; set; }
             public WeightCategories MaxWeight { get; set; }
