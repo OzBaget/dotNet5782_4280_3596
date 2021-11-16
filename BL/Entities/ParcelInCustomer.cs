ede9c8ab38a5c41ffa1050736bc9cd700d0bedd2
Oz Diei <oz052777@gmail.com>
2021-11-16 19:45:14 +0200

    Finish all ToString in BL

--- BL/Entities/ParcelInCustomer.cs
+++ BL/Entities/ParcelInCustomer.cs
@@ -13,5 +13,14 @@ namespace IBL.BO
         public Priorities Prioritie;
         public ParcelStatus StatusParcel;
         public CustomerInParcel Customer;
+        public override string ToString()
+        {
+            string toString =
+                            $"ID:            {Id}\n" +
+                            $"Weight status:      {Weight}\n" +
+                            $"Prioritie status:      {Prioritie}\n" +
+                            $"Parcel status:      {StatusParcel}\n" +
+                            $"Customer :      {Customer}\n";                    
+            return toString;
+        }
     }
-}
--- BL/Entities/ParcelInTransfer.cs
+++ BL/Entities/ParcelInTransfer.cs
@@ -17,5 +17,21 @@ namespace IBL.BO
         public Location PickupLocation;
         public Location TargetLocation;
         public double Distance;
+        public override string ToString()
+        {
+            string toString =
+                $"ID:            {Id}\n" +
+                $"Is in transer:            {IsInTransfer}\n" +
+                $"Prioritie status:      {Prioritie}\n" +
+                $"Weight status:      {Weight}\n" +
+                $"Receiver:     {Target}\n" +
+                $"Sender:          {Sender}\n" +
+                $"Pick up location:          {PickupLocation}\n" +
+                $"Target location:          {TargetLocation}\n" +
+                $"Distance frome target:          {Distance}\n";
+                
+
+            return toString;
+        }
     }
 }
